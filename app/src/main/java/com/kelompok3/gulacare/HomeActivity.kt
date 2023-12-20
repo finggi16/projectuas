2524c6a5fd941028f8b2df87d4222d63983d9485
projectuas <finggi21ti@mahasiswa.pcr.ac.id>
2023-12-20 17:28:04 +0700

    finished project

---
+++ app/src/main/java/com/kelompok3/gulacare/HomeActivity.kt
@@ -0,0 +1,41 @@
+package com.kelompok3.gulacare
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kelompok3.gulacare.databinding.ActivityHomeBinding
+import com.kelompok3.gulacare.view.ListRetrofit
+
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.homeProfile.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.profilePage.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.searchPage.setOnClickListener {
+            val intent = Intent(this, ListRetrofit::class.java)
+            startActivity(intent)
+        }
+
+        binding.homePage.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
